1402215b0309d04227e4c7d687d4e94a112ef482
rob <robmitchell@email.com>
2018-06-22 10:15:51 +0200

    Create method working with links to show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,10 +6,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.book = Book.find(params[:book_id])
+    @review.save
+    redirect_to book_path(@book)
   end
 
   private
   def set_book
     @book = Book.find(params[:book_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
